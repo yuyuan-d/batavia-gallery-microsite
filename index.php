5532f6b5daba3b58a3d869d52279069f89daf891
Yuyuan <yuyuanduan@gmail.com>
2022-06-02 17:26:35 +0800

    wp

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php get_header();?>   <!--  Tells WordPress to include header.php -->
+
+ <section class="container-fluid bg2">
+
+    <div class="row">
+    <div class="col-md-4">
+    </div> 
+
+    <div class="col history">
+    <h3>ONE OF THE WORST HORROR STORIES IN MARITIME HISTORY</h3>
+    <p>The journey had an inauspicious start. A violent storm on the North Sea separated most of the ships in the convoy, and when calmer weather returned only three of the seven ships had sight of each other; Batavia, Assendelft, and the Buren. </p>
+    <a href="history.html" class="btn" role="button">Learn More</a>
+    </div>
+
+    </div> <!-- row -->
+ </section>
+
+ <section class="container-fluid visit-section">
+
+    <div class="row">
+    <div class="col-md-6 visit">
+    <h2>PLAN A VISIT</h2>
+    <p>Whether you’re a parent looking to take your child somewhere special or a school teacher looking for an excursion destination. Plan a visit at the WA Shipwrecks museum today and learn about the historical story of the batavia shipwreck.</p>
+    <a href="#" class="btn" role="button">Book Tickets</a>
+  </div>
+
+  <div class="col-md-6 bg3">
+  <!-- full-width image -->
+  </div> 
+
+    </div> <!-- row -->
+</section>
+
+<?php get_footer();?>   <!-- Tells WordPress to include footer.php   -->
