5cab63dad2d53a46b5295bdefd2c5142e50b1c8e
Yuyuan <yuyuanduan@gmail.com>
2022-06-02 17:25:56 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="container-fluid">
+  <div class="container footer-nav col-md-8">
+      <div class="row">
+        <div class="col-md-3">
+        <a class="footer-link" href="#">Terms & conditions</a>
+      </div>
+
+        <div class="col-md-3">
+        <a class="footer-link" href="#">Privacy policy</a>
+      </div>
+
+        <div class="col-md-3">
+          <a class="footer-link" href="#">Disclaimer</a>
+        </div>
+
+          <div class="col-md-3">
+          <a class="footer-link" href="#">Contact Us</a>
+        </div>
+     </div> <!-- row -->
+
+    <div class="container">
+      <p>© 2022  WA Shipwrecks Museum. All Rights Reserved.</p>
+    </div>
+   
+
+  </div> <!-- container -->
+</footer>
+<?php wp_footer(); ?>
+
+</body>
+</html>
